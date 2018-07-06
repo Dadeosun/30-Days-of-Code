f74958518881f02750374d8e57003b317a4ce052
Subiksha Sureender <subikshasureender@gmail.com>
2018-07-06 12:45:02 -0400

    day 1 added

---
+++ 30 Days of Code - Swift/Day 1 - Data Types.playground/Contents.swift
@@ -0,0 +1,25 @@
+
+
+//Code by Subiksha Sureender
+
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+// Declaring, reading and saving second integer, double, and String variables.
+
+let ii = readLine()!
+let dd = readLine()!
+let ss = readLine()!
+
+
+let sum1 = i + Int(ii)!  //calculating sum of two integers
+let sum2 = d + Double(dd)!  //calculating sum of two doubles
+
+
+print(sum1) // printng sum of integers
+print(sum2) // printing sum of doubles
+print(s+ss) //printing concatenated strings
+
+
+
