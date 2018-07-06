bc083384eb4ba51b54d0de675bda42f118f066d7
Subiksha Sureender <subikshasureender@gmail.com>
2018-07-06 12:18:10 -0400

    day 0
    day 0

---
+++ 30 Days of Code - Swift/Day 0 - Hello World.playground/Contents.swift
@@ -0,0 +1,13 @@
+
+
+// Code by Subiksha Sureender
+
+
+// Gets a line of input from stdin and saves it to our variable
+let inputString = readLine()!
+
+// Prints Hello,World
+print("Hello, World.")
+
+// Prints the string stored in inutString variable
+print(inputString)
