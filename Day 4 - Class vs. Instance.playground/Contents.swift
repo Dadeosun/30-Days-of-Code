ae9c3b50f317b20df3714ebf1d533d5bfa784ffc
Subiksha Sureender <subikshasureender@gmail.com>
2018-07-24 11:04:04 +0530

    day 4
    day 4

---
+++ Day 4 - Class vs. Instance.playground/Contents.swift
@@ -0,0 +1,43 @@
+
+//Code by Subiksha Sureender
+
+class Person {
+    var age: Int = 0
+    
+    init(initialAge: Int) {
+        
+        
+        if (initialAge>0)
+        {
+            age = initialAge
+        }
+        else
+        {
+            age = 0
+            print("Age is not valid, setting age to 0.")
+        }
+    }
+    
+    
+    
+    func amIOld() {
+        // Do some computations in here and print out the correct statement to the console
+        if(age<13)
+        {
+            print("You are young.")
+        }
+        else if (age>12 && age<18)
+        {
+            print("You are a teenager.")
+        }
+        else
+        {
+            print("You are old.")
+        }
+    }
+    
+    func yearPasses() {
+        // Increment the age of the person in here
+        age+=1
+    }
+}
